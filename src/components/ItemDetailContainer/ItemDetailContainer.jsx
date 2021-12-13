5e15599a648ec1200cad81256ea1af0a6e12e2b6
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2021-12-13 20:59:06 -0300

    Update ItemDetailContainer.jsx

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,14 +1,15 @@
-import React , {useState , useEffect, useContext} from "react";
+import React , { useEffect, useContext} from "react";
 import { useParams } from "react-router";
 import {products} from '../ItemListContainer/Items'
 import ItemDetail from '../ItemDetail/ItemDetail'
 import CartContext from '../../context/CartContext'
+import { getFirestore, doc, getDoc } from 'firebase/firestore'
 
 const ItemDetailContainer = () => {
-     const { items, setItems } = useContext(CartContext)
+    const { items, setItems } = useContext(CartContext)
     // const [items, setItems] = useState([]);
     const { codigoId } = useParams()
-   
+
     useEffect(() => {
         const getItem = new Promise((resolve, reject) => {
             // setTimeout(() => {
@@ -22,7 +23,22 @@ const ItemDetailContainer = () => {
                     : setItems(resp)
             })
         
-    }, [codigoId]);
+    }, [codigoId, setItems]);
+
+    useEffect(() => {
+            const db = getFirestore()
+            const ref = doc(db, 'products', "ygNGtHtie8voxautNGLy")
+        
+            getDoc(ref)
+                .then((snapShop) => {
+                if(snapShop.exists()){
+                    // setItems(snapShop.data())
+                    // const product = snapShop.data();
+                    console.log(snapShop.data())
+                }
+            })
+
+        }, [])
     return (
         <>
             {items.map((item) => {
