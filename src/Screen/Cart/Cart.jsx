0c48dd436d01991647162c2179af07de420016d1
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2021-12-13 21:41:47 -0300

    Update Cart.jsx

--- src/Screen/Cart/Cart.jsx
+++ src/Screen/Cart/Cart.jsx
@@ -34,7 +34,7 @@ const Cart = () => {
                     ?(<>
                         <Header />
                         <div className="container-titulo">
-                        <Link to="/productos" className="atras"><i class="far fa-arrow-alt-circle-left"></i></Link>
+                        <Link to="/productos" className="atras"><i className="far fa-arrow-alt-circle-left"></i></Link>
                         <h1 className="tituloCarrito">Carrito de Compras Vacio</h1>
                         </div>
                     </>)
@@ -59,7 +59,7 @@ const Cart = () => {
                                                 <span className="cart-title">{name}</span>
                                                 <span className="cart-detail">{detail}</span>
                                                 <p className="cart-price">Precio $ <span>{price}</span></p>
-                                                <button className="cart-button" onClick={() => deleteItem(item)}><i class="fas fa-trash"></i></button>
+                                                <button className="cart-button" onClick={() => deleteItem(item)}><i className="fas fa-trash"></i></button>
                                                 <div className="cart-amount">
                                                     <button className="cart-button-decrease" onClick={() => decrease(id)}> - </button>
                                                     <span className="cart-count">{count}</span>
@@ -70,8 +70,8 @@ const Cart = () => {
                                            
                                                 <div className="cart-botones">
                                                     {/* <button className="cart-button-pagar" onClick={() => sort_lists(item)}>Ordenar Categoria</button> */}
-                                                    <button className="cart-button-eliminar" onClick={() => deleteItemsCategory(item)}><i class="fas fa-eraser"></i>Eliminar Categoria</button>
-                                                    <button className="cart-button-limpiar" onClick={() => deleteItems()}><i class="fas fa-trash-alt"></i>Limpiar</button>
+                                                    <button className="cart-button-eliminar" onClick={() => deleteItemsCategory(item)}><i className="fas fa-eraser"></i>Eliminar Categoria</button>
+                                                    <button className="cart-button-limpiar" onClick={() => deleteItems()}><i className="fas fa-trash-alt"></i>Limpiar</button>
                                                 </div>
                                             </div>
                                             </>
