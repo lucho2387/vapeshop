9ea66c546824e452f6e98421a1519793403d6ecd
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2021-12-13 21:09:54 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
