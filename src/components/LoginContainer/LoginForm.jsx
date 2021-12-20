f97ea9a9b13ba2a305d385c1d5258fd7aa473af6
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2021-12-20 18:03:06 -0300

    Update LoginForm.jsx

--- src/components/LoginContainer/LoginForm.jsx
+++ src/components/LoginContainer/LoginForm.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react'
+// import { Link } from 'react-router-dom'
 import './loginForm.css'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 
 const LoginForm = (props) => {
+
+    const [loginUser, setLoginUser] = useState()
+    const auth = getAuth()
+
+    const onChangeLogIn = (event) => {
+        setLoginUser({
+            ...loginUser,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const onSubmitLogIn = (e) => {
+        e.preventDefault()
+        if(loginUser.email !== '' && loginUser.password !== ''){
+            signInWithEmailAndPassword(auth, loginUser?.email, loginUser?.password)
+                .then((userCredential) => {
+                    console.log(userCredential)
+                })
+        }else {
+            alert("Hay campos vacios")
+        }   
+    }
+
     return (
-        <>
+        <>  
             <div className="container-caja1">
                 <form className="container-form">
-                    <Link to="/registro" className="form-enlace1">Registrarse</Link>
-                    <Link to="/" className="form-enlace2"><i class="fas fa-long-arrow-alt-left"></i></Link>
-                    <input className="form-input" type="email" placeholder="Correo" />
-                    <input className="form-input" type="password" placeholder="Contraseña"/>
-                    <button type="submit" className="form-boton">Ingresar</button>
+                    <a href="/registro" className="form-enlace1">Registrarse</a>
+                    {/* <a href="/" className="form-enlace2"><i class="fas fa-long-arrow-alt-left"></i></a> */}
+                    <input className="form-input" type="email" placeholder="Correo" name='email' onChange={onChangeLogIn} />
+                    <input className="form-input" type="password" placeholder="Contraseña" name='password' onChange={onChangeLogIn}/>
+                    <button type="submit" className="form-boton" onClick={onSubmitLogIn}>Ingresar</button>
                 </form>
             </div>
         </>
