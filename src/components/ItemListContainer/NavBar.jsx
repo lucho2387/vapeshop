1bba1bee435aa9b4a858a12ba0b3e7cdb0415471
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2021-12-20 18:00:57 -0300

    Update NavBar.jsx

--- src/components/ItemListContainer/NavBar.jsx
+++ src/components/ItemListContainer/NavBar.jsx
@@ -2,19 +2,26 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import './itemListContainer.css'
 
-const NavBar = () => {
+const NavBar = ({ categories, filterItems }) => {
     return (
+      
       <div className="new_arrivals">
         <div className="containerNavegacion">
           <div className="row">
               <div className="col">
                 <div className="new_arrivals_sorting">
-                  <ul className="arrivals_grid_sorting">
-                    <li className="grid_sorting_button"><Link to="/productos/vaporizadores">Vaporizadores</Link></li>
-                    <li className="grid_sorting_button "><Link to="/productos/atomizadores" >Atomizadores</Link></li>
-                    <li className="grid_sorting_button "><Link to="/productos/cartuchos" >Cartuchos</Link></li>
-                    <li className="grid_sorting_button "><Link to="/productos/accesorios">Accesorios</Link></li>
-                  </ul>
+                  <div className="arrivals_grid_sorting">
+                    {
+                      categories.map((category, index) => {
+                        return  <button type='button' className="grid_sorting_button"
+                                  key={index}
+                                  onClick={() => filterItems(category)}>
+                                  <Link to={`/productos/${category}`}>{category}</Link>
+                                </button>
+                      })
+                    }
+                      
+                  </div>
                 </div>
               </div>
           </div>    
