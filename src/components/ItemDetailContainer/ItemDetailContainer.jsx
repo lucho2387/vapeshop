c990da226287ff6a0004e3c5d08dd4676aa1d720
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2021-12-20 17:58:04 -0300

    Update ItemDetailContainer.jsx

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -3,13 +3,14 @@ import { useParams } from "react-router";
 import {products} from '../ItemListContainer/Items'
 import ItemDetail from '../ItemDetail/ItemDetail'
 import CartContext from '../../context/CartContext'
-import { getFirestore, doc, getDoc } from 'firebase/firestore'
+// import { getFirestore, doc, getDoc } from 'firebase/firestore'
 
 const ItemDetailContainer = () => {
     const { items, setItems } = useContext(CartContext)
     // const [items, setItems] = useState([]);
     const { codigoId } = useParams()
-
+   
+    console.log(codigoId)
     useEffect(() => {
         const getItem = new Promise((resolve, reject) => {
             // setTimeout(() => {
@@ -23,27 +24,33 @@ const ItemDetailContainer = () => {
                     : setItems(resp)
             })
         
-    }, [codigoId, setItems]);
+    }, [codigoId]);
 
-    useEffect(() => {
-            const db = getFirestore()
-            const ref = doc(db, 'products', "ygNGtHtie8voxautNGLy")
+
+
+    // useEffect(() => {
+
+    //         const db = getFirestore()
+    //         const ref = doc(db, 'productos', codigoId)
         
-            getDoc(ref)
-                .then((snapShop) => {
-                if(snapShop.exists()){
-                    // setItems(snapShop.data())
-                    // const product = snapShop.data();
-                    console.log(snapShop.data())
-                }
-            })
+    //         getDoc(ref)
+    //             .then((snapShop) => {
+    //             if(snapShop.exists()){
+    //                 // setItems(snapShop.data())
+    //                 // const product = snapShop.data();
+    //                 // console.log(snapShop.data())
+    //                 // setItems(snapShop.doc.find((doc) => ({id: doc.id, ...doc.data()})))
+    //                 // setItems(snapShop.data().id)
+    //                 setItems(snapShop.docs.filter(product => product.id === codigoId))
+    //             }
+    //         })
 
-        }, [])
+    //     }, [codigoId])
     return (
         <>
-            {items.map((item) => {
+            {items.map((item,index) => {
                 return(
-                        <div key={item.id}>
+                        <div key={index}>
                             <ItemDetail item={item} />
                     </div>
                 ) 
