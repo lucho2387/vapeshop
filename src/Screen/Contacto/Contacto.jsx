2ce4e56266c6df3b322f96f1377d3620fc23a74f
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2021-12-27 11:49:59 -0300

    Update Contacto.jsx

--- src/Screen/Contacto/Contacto.jsx
+++ src/Screen/Contacto/Contacto.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import './contacto.css'
+import Footer from '../../components/Footer/Footer'
 
 const Contacto = () => {
     return (
         <>
             <Header />
             <h1 className="tituloContacto">Contacto</h1>
+            <Footer />
         </>
      )
 }
