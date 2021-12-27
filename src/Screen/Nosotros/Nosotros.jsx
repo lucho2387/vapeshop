a102b0fe8e7679383bae24ad92ea0aad6ea3e492
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2021-12-27 11:51:27 -0300

    Update Nosotros.jsx

--- src/Screen/Nosotros/Nosotros.jsx
+++ src/Screen/Nosotros/Nosotros.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import './nosotros.css'
+import Footer from '../../components/Footer/Footer'
 
 const Nosotros = () => {
     return (
         <>
             <Header />
             <h1 className="tituloNosotros">Nosotros</h1>
+            <Footer />
         </>
      )
 }
