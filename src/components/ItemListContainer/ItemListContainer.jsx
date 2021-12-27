4d6c5e3d8a896bb2075da3a094ba07c108f95a2d
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2021-12-27 11:44:15 -0300

    Update ItemListContainer.jsx

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,27 +2,22 @@ import React, {useState, useEffect} from 'react'
 import {products} from '../ItemListContainer/Items'
 import ItemList from '../ItemList/ItemList';
 import './itemListContainer.css'
-// import { useParams } from 'react-router-dom';
 import NavBar from './NavBar';
 import { getFirestore, collection, getDocs, query, where } from 'firebase/firestore' 
 
 
 const allCategories = ['all', ...new Set(products.map((item) => item.category))]
-// console.log(allCategories)
+
 const ItemListContainer = () => {
   
-  const [items, setItems] = useState([])
-  const [categories, setCategories] = useState(allCategories)
- 
+    const [items, setItems] = useState([])
+    const [categories, setCategories] = useState(allCategories)
 
-  const filterItems = (category) => {
-    if (category === 'all') {
-      setItems(products)
-      return
-    }
-    // const newItems = products.filter((item) => 
-    //   item.category === category)
-    //   setItems(newItems)
+    const filterItems = (category) => {
+      if (category === 'all') {
+        setItems(products)
+        return
+      }
     const db = getFirestore() 
     const refItems = collection(db, "productos")
     const queryItem = query(refItems, where("category", "==" , category))
@@ -54,3 +49,4 @@ const ItemListContainer = () => {
 }
 
 export default ItemListContainer
+
