fd73de683cc8f0f13144ad21a662188fd7d4e35b
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-03-22 20:02:29 -0300

    Update ItemCount.jsx

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -6,26 +6,26 @@ import { useAddToCart } from '../../context/CartContext'
 const ItemCount = ({ item, stock, price }) => {
     
 
-    // const [numero, setNumero] = useState(1)
+    const [numero, setNumero] = useState(1)
 
-    // const onIncrease = () => {
-    //     setNumero(numero < stock ? numero + 1 : numero);
-    // }
+    const onIncrease = () => {
+        setNumero(numero < stock ? numero + 1 : numero);
+    }
 
-    // const onDecrease = () => {
-    //     setNumero(numero > 1 ? numero - 1 : numero);
-    // }
+    const onDecrease = () => {
+        setNumero(numero > 1 ? numero - 1 : numero);
+    }
     
     const addToCart = useAddToCart()
 
     return (
         <>
         <div className="container">
-            {/* <button className="botonDisminuir1"  onClick={onDecrease}>-</button>
+            {<button className="botonDisminuir1"  onClick={onDecrease}>-</button>
             <h3 className="cantidad1">{`${numero}`}</h3>
-            <button className="botonIncrementar1" onClick={onIncrease}>+</button> */}
+            <button className="botonIncrementar1" onClick={onIncrease}>+</button>}
             {/* <h4 className="precioProducto1">Total $ <span className="spanPrecio1">{price * numero}</span></h4> */}
-            <button className="botonAgregar1"  onClick={() => addToCart(item)}>Agregar</button>
+            <button className="botonAgregar1"  onClick={() => addToCart(item,numero)}>Agregar</button>
         </div>
         </>
     )
